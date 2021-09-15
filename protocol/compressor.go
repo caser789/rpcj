e523fe58d2a053ba655de7e62ec831cc0c1c9ae9
Xue Jiao <jiao.xue@seamoney.com>
2021-09-15 20:11:29 +0800

    snappy compressor

--- protocol/compressor.go
+++ protocol/compressor.go
@@ -1,6 +1,12 @@
 package protocol
 
-import "github.com/caser789/rpcj/util"
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/caser789/rpcj/util"
+	"github.com/golang/snappy"
+)
 
 // Compressor defines a common compression interface.
 type Compressor interface {
@@ -29,4 +35,42 @@ func (c RawDataCompressor) Zip(data []byte) ([]byte, error) {
 
 func (c RawDataCompressor) Unzip(data []byte) ([]byte, error) {
 	return data, nil
+}
+
+// SnappyCompressor implements snappy compressor
+type SnappyCompressor struct {
+}
+
+func (c *SnappyCompressor) Zip(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return data, nil
+	}
+
+	var buffer bytes.Buffer
+	writer := snappy.NewWriter(&buffer)
+	_, err := writer.Write(data)
+	if err != nil {
+		writer.Close()
+		return nil, err
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+func (c *SnappyCompressor) Unzip(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return data, nil
+	}
+
+	reader := snappy.NewReader(bytes.NewReader(data))
+	out, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, err
 }
